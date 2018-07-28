268809b3ac840514d28c1ecc3a45119a15708ff8
Alex Barney <thealexbarney@gmail.com>
2018-07-28 13:33:34 -0500

    Net project

---
+++ Net/Options.cs
@@ -0,0 +1,22 @@
+﻿using libhac;
+
+namespace Net
+{
+    internal class Options
+    {
+        public string Keyfile;
+        public string TitleKeyFile;
+        public string ConsoleKeyFile;
+        public ulong TitleId;
+        public int Version;
+        public ulong DeviceId;
+        public string CertFile;
+    }
+
+    internal class Context
+    {
+        public Options Options;
+        public Keyset Keyset;
+        public IProgressReport Logger;
+    }
+}
