ec1d5134ac031600c1b8c8a9fad7cc99491fb7a5
caitsith2 <d_good@caitsith2.com>
2018-08-25 15:22:16 -0700

    Fix case sensitivity issues with long filenames.

--- libhac.Nand/DiscUtils.Fat/FileName.cs
+++ libhac.Nand/DiscUtils.Fat/FileName.cs
@@ -131,14 +131,14 @@ namespace DiscUtils.Fat
                 return false;
             }
             if (!string.IsNullOrEmpty(LongName) && !string.IsNullOrEmpty(other.LongName))
-                return LongName == other.LongName;
+                return LongName.Equals(other.LongName, StringComparison.InvariantCultureIgnoreCase);
 
             return CompareRawNames(this, other) == 0;
         }
 
         public static FileName FromPath(string path, Encoding encoding)
         {
-            return new FileName(Utilities.GetFileFromPath(path), encoding, false);
+            return new FileName(Utilities.GetFileFromPath(path), encoding, true);
         }
 
         public static bool operator ==(FileName a, FileName b)
