8a7729cb51d40a5fc95545333d6bf04ddf3b1368
Alex Barney <thealexbarney@gmail.com>
2019-01-12 20:23:34 -0600

    Add SubdirectoryFileSystem

---
+++ src/LibHac/IO/SubdirectoryFileSystemDirectory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LibHac.IO
+{
+    public class SubdirectoryFileSystemDirectory : IDirectory
+    {
+        public SubdirectoryFileSystemDirectory(SubdirectoryFileSystem fs, IDirectory baseDir, string path, OpenDirectoryMode mode)
+        {
+            ParentFileSystem = fs;
+            BaseDirectory = baseDir;
+            FullPath = path;
+            Mode = mode;
+        }
+
+        public IFileSystem ParentFileSystem { get; }
+        public string FullPath { get; }
+        public OpenDirectoryMode Mode { get; }
+
+        private IDirectory BaseDirectory { get; }
+
+        public IEnumerable<DirectoryEntry> Read()
+        {
+            return BaseDirectory.Read();
+        }
+
+        public int GetEntryCount()
+        {
+            return BaseDirectory.GetEntryCount();
+        }
+    }
+}
