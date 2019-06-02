2c7e0d58c53318153633dad8fbed29260270dd92
Alex Barney <thealexbarney@gmail.com>
2019-06-02 00:48:21 -0500

    Add Result struct

---
+++ src/LibHac/Result.cs
@@ -0,0 +1,28 @@
+﻿namespace LibHac
+{
+    public struct Result
+    {
+        public readonly int Value;
+
+        public Result(int value)
+        {
+            Value = value;
+        }
+
+        public Result(int module, int description)
+        {
+            Value = (description << 9) | module;
+        }
+
+        public int Description => (Value >> 9) & 0x1FFF;
+        public int Module => Value & 0x1FF;
+
+        public bool IsSuccess() => Value == 0;
+        public bool IsFailure() => Value != 0;
+    }
+
+    public static class Results
+    {
+        public static readonly Result ResultSuccess = new Result(0);
+    }
+}
