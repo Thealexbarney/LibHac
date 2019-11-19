4b2c4d9553e514674bf93d1d040069716c5c1b74
Alex Barney <thealexbarney@gmail.com>
2019-11-18 18:08:25 -0700

    Refactor non-NI AES code

---
+++ src/LibHac/Common/RentedArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+
+namespace LibHac.Common
+{
+    public readonly ref struct RentedArray<T>
+    {
+        // It's faster to create new smaller arrays than rent them
+        private const int RentThresholdBytes = 512;
+        private static int RentThresholdElements => RentThresholdBytes / Unsafe.SizeOf<T>();
+
+        private readonly Span<T> _span;
+
+        public T[] Array { get; }
+        public Span<T> Span => _span;
+
+        public RentedArray(int minimumSize)
+        {
+            if (minimumSize >= RentThresholdElements)
+            {
+                Array = ArrayPool<T>.Shared.Rent(minimumSize);
+            }
+            else
+            {
+                Array = new T[minimumSize];
+            }
+
+            _span = Array.AsSpan(0, minimumSize);
+        }
+
+        public void Dispose()
+        {
+            // Only return if array was rented
+            if (_span.Length >= RentThresholdElements)
+            {
+                ArrayPool<T>.Shared.Return(Array);
+            }
+        }
+    }
+}
