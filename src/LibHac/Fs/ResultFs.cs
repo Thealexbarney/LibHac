f44b09ec1f5ccb33b0cdf05636a2d8d90c029d27
Alex Barney <thealexbarney@gmail.com>
2020-03-23 09:10:55 -0700

    Add some Results

--- src/LibHac/Fs/ResultFs.cs
+++ src/LibHac/Fs/ResultFs.cs
@@ -313,6 +313,8 @@ namespace LibHac.Fs
                 public static Result.Base InvalidSize => new Result.Base(ModuleFs, 6062);
                 /// <summary>Error code: 2002-6063; Inner value: 0x2f5e02</summary>
                 public static Result.Base NullptrArgument => new Result.Base(ModuleFs, 6063);
+                /// <summary>Error code: 2002-6064; Inner value: 0x2f6002</summary>
+                public static Result.Base InvalidAlignment => new Result.Base(ModuleFs, 6064);
                 /// <summary>Error code: 2002-6065; Inner value: 0x2f6202</summary>
                 public static Result.Base InvalidMountName => new Result.Base(ModuleFs, 6065);
                 /// <summary>Error code: 2002-6066; Inner value: 0x2f6402</summary>
@@ -320,7 +322,13 @@ namespace LibHac.Fs
                 /// <summary>Error code: 2002-6067; Inner value: 0x2f6602</summary>
                 public static Result.Base ExtensionSizeInvalid => new Result.Base(ModuleFs, 6067);
                 /// <summary>Error code: 2002-6068; Inner value: 0x2f6802</summary>
-                public static Result.Base ReadOldSaveDataInfoReader => new Result.Base(ModuleFs, 6068);
+                public static Result.Base InvalidSaveDataInfoReader => new Result.Base(ModuleFs, 6068);
+                /// <summary>Error code: 2002-6069; Inner value: 0x2f6a02</summary>
+                public static Result.Base InvalidCacheStorageSize => new Result.Base(ModuleFs, 6069);
+                /// <summary>Error code: 2002-6070; Inner value: 0x2f6c02</summary>
+                public static Result.Base InvalidCacheStorageIndex => new Result.Base(ModuleFs, 6070);
+                /// <summary>Up to 10 file systems can be committed at the same time.<br/>Error code: 2002-6071; Inner value: 0x2f6e02</summary>
+                public static Result.Base InvalidCommitNameCount => new Result.Base(ModuleFs, 6071);
 
                 /// <summary>Error code: 2002-6080; Range: 6080-6099; Inner value: 0x2f8002</summary>
                 public static Result.Base InvalidEnumValue { [MethodImpl(MethodImplOptions.AggressiveInlining)] get => new Result.Base(ModuleFs, 6080, 6099); }
