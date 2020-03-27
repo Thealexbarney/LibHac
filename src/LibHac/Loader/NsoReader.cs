f59c7c6a840b4db3e0d38133deb378a991113e80
Alex Barney <thealexbarney@gmail.com>
2020-03-26 22:49:35 -0700

    Add NsoReader

---
+++ src/LibHac/Loader/NsoReader.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using LibHac.Common;
+using LibHac.Fs;
+
+namespace LibHac.Loader
+{
+    public class NsoReader
+    {
+        private IFile NsoFile { get; set; }
+
+        public NsoHeader Header;
+
+        public Result Initialize(IFile nsoFile)
+        {
+            Result rc = nsoFile.Read(out long bytesRead, 0, SpanHelpers.AsByteSpan(ref Header), ReadOption.None);
+            if (rc.IsFailure()) return rc;
+
+            if (bytesRead != Unsafe.SizeOf<NsoHeader>())
+                return ResultLoader.InvalidNso.Log();
+
+            NsoFile = nsoFile;
+            return Result.Success;
+        }
+
+        public Result GetSegmentSize(SegmentType segment, out uint size)
+        {
+            switch (segment)
+            {
+                case SegmentType.Text:
+                case SegmentType.Ro:
+                case SegmentType.Data:
+                    size = Header.Segments[(int)segment].Size;
+                    return Result.Success;
+                default:
+                    size = default;
+                    return ResultLibHac.ArgumentOutOfRange.Log();
+            }
+        }
+
+        public Result ReadSegment(SegmentType segment, Span<byte> buffer)
+        {
+            Result rc = GetSegmentSize(segment, out uint segmentSize);
+            if (rc.IsFailure()) return rc;
+
+            if (buffer.Length < segmentSize)
+                return ResultLibHac.BufferTooSmall.Log();
+
+            bool isCompressed = (((int)Header.Flags >> (int)segment) & 1) != 0;
+            bool checkHash = (((int)Header.Flags >> (int)segment) & 8) != 0;
+
+            return ReadSegmentImpl(ref Header.Segments[(int)segment], Header.CompressedSizes[(int)segment],
+                Header.SegmentHashes[(int)segment], isCompressed, checkHash, buffer);
+        }
+
+        private Result ReadSegmentImpl(ref NsoHeader.SegmentHeader segment, uint fileSize, Buffer32 fileHash,
+            bool isCompressed, bool checkHash, Span<byte> buffer)
+        {
+            // Select read size based on compression.
+            if (!isCompressed)
+            {
+                fileSize = segment.Size;
+            }
+
+            // Validate size.
+            if (fileSize > segment.Size)
+                return ResultLoader.InvalidNso.Log();
+
+            // Load data from file.
+            uint loadAddress = isCompressed ? (uint)buffer.Length - fileSize : 0;
+
+            Result rc = NsoFile.Read(out long bytesRead, segment.FileOffset, buffer.Slice((int)loadAddress), ReadOption.None);
+            if (rc.IsFailure()) return rc;
+
+            if (bytesRead != fileSize)
+                return ResultLoader.InvalidNso.Log();
+
+            // Uncompress if necessary.
+            if (isCompressed)
+            {
+                Lz4.Decompress(buffer.Slice((int)loadAddress), buffer);
+            }
+
+            // Check hash if necessary.
+            if (checkHash)
+            {
+                Buffer32 hash = default;
+                Crypto.Sha256.GenerateSha256Hash(buffer.Slice(0, (int)segment.Size), hash.Bytes);
+
+                if (hash.Bytes.SequenceCompareTo(fileHash.Bytes) != 0)
+                    return ResultLoader.InvalidNso.Log();
+            }
+
+            return Result.Success;
+        }
+
+        public enum SegmentType
+        {
+            Text = 0,
+            Ro = 1,
+            Data = 2
+        }
+    }
+}
--- src/LibHac/Lz4.cs
+++ src/LibHac/Lz4.cs
@@ -76,5 +76,72 @@ namespace LibHac
 
             return dec;
         }
+
+        public static void Decompress(ReadOnlySpan<byte> cmp, Span<byte> dec)
+        {
+            int cmpPos = 0;
+            int decPos = 0;
+
+            int GetLength(int length, ReadOnlySpan<byte> cmp)
+            {
+                byte sum;
+
+                if (length == 0xf)
+                {
+                    do
+                    {
+                        length += sum = cmp[cmpPos++];
+                    }
+                    while (sum == 0xff);
+                }
+
+                return length;
+            }
+
+            do
+            {
+                byte token = cmp[cmpPos++];
+
+                int encCount = (token >> 0) & 0xf;
+                int litCount = (token >> 4) & 0xf;
+
+                //Copy literal chunk
+                litCount = GetLength(litCount, cmp);
+
+                cmp.Slice(cmpPos, litCount).CopyTo(dec.Slice(decPos));
+
+                cmpPos += litCount;
+                decPos += litCount;
+
+                if (cmpPos >= cmp.Length)
+                {
+                    break;
+                }
+
+                //Copy compressed chunk
+                int back = cmp[cmpPos++] << 0 |
+                           cmp[cmpPos++] << 8;
+
+                encCount = GetLength(encCount, cmp) + 4;
+
+                int encPos = decPos - back;
+
+                if (encCount <= back)
+                {
+                    dec.Slice(encPos, encCount).CopyTo(dec.Slice(decPos));
+
+                    decPos += encCount;
+                }
+                else
+                {
+                    while (encCount-- > 0)
+                    {
+                        dec[decPos++] = dec[encPos++];
+                    }
+                }
+            }
+            while (cmpPos < cmp.Length &&
+                   decPos < dec.Length);
+        }
     }
 }
