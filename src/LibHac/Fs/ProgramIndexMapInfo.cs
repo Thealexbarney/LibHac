d2afe88e9ed1a7fcb7f60d90b0461fd7e6131390
Alex Barney <thealexbarney@gmail.com>
2020-08-21 18:47:58 -0700

    Add ProgramIndexMapInfoManager

---
+++ src/LibHac/Fs/ProgramIndexMapInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+using LibHac.Ncm;
+
+namespace LibHac.Fs
+{
+    [StructLayout(LayoutKind.Explicit, Size = 0x20)]
+    public struct ProgramIndexMapInfo
+    {
+        [FieldOffset(0x00)] public ProgramId ProgramId;
+        [FieldOffset(0x08)] public ProgramId MainProgramId;
+        [FieldOffset(0x10)] public byte ProgramIndex;
+    }
+}
