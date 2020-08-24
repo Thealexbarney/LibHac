d9549498e46f223ed06bf806667213af42162520
Alex Barney <thealexbarney@gmail.com>
2020-08-23 19:33:25 -0700

    Add IFileSystemProxyForLoader and IProgramRegistry interfaces

---
+++ src/LibHac/Fs/CodeVerificationData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using LibHac.Common;
+
+namespace LibHac.Fs
+{
+    [StructLayout(LayoutKind.Explicit, Size = 0x124)]
+    public struct CodeVerificationData
+    {
+        private const int Signature2Size = 0x100;
+
+        [FieldOffset(0x000)] private byte _signature2;
+        [FieldOffset(0x100)] public Buffer32 NcaHeaderHash;
+        [FieldOffset(0x120)] public bool IsValid;
+
+        public Span<byte> NcaSignature2 => SpanHelpers.CreateSpan(ref _signature2, Signature2Size);
+    }
+}
---
+++ src/LibHac/Fs/Shim/ProgramRegistry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using LibHac.FsSrv;
+using LibHac.FsSrv.Sf;
+using LibHac.Ncm;
+
+namespace LibHac.Fs.Shim
+{
+    public static class ProgramRegistry
+    {
+        /// <inheritdoc cref="ProgramRegistryImpl.RegisterProgram"/>
+        public static Result RegisterProgram(this FileSystemClient fs, ulong processId, ProgramId programId,
+            StorageId storageId, ReadOnlySpan<byte> accessControlData, ReadOnlySpan<byte> accessControlDescriptor)
+        {
+            IProgramRegistry registry = fs.GetProgramRegistryServiceObject();
+
+            Result rc = registry.SetCurrentProcess(fs.Hos.ProcessId.Value);
+            if (rc.IsFailure()) return rc;
+
+            return registry.RegisterProgram(processId, programId, storageId, accessControlData,
+                accessControlDescriptor);
+        }
+
+        /// <inheritdoc cref="ProgramRegistryImpl.UnregisterProgram"/>
+        public static Result UnregisterProgram(this FileSystemClient fs, ulong processId)
+        {
+            IProgramRegistry registry = fs.GetProgramRegistryServiceObject();
+
+            Result rc = registry.SetCurrentProcess(fs.Hos.ProcessId.Value);
+            if (rc.IsFailure()) return rc;
+
+            return registry.UnregisterProgram(processId);
+        }
+    }
+}
