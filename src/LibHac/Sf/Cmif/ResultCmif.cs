d1a7dbc7cfa6cc483b36f0947d534a413a0fa426
Alex Barney <thealexbarney@gmail.com>
2021-01-08 20:56:48 -0700

    Move Results to their correct namespaces

---
+++ src/LibHac/Sf/Cmif/ResultCmif.cs
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------------------------------------
+// This file was automatically generated.
+// Changes to this file will be lost when the file is regenerated.
+//
+// To change this file, modify /build/CodeGen/results.csv at the root of this
+// repo and run the build script.
+//
+// The script can be run with the "codegen" option to run only the
+// code generation portion of the build.
+//-----------------------------------------------------------------------------
+
+namespace LibHac.Sf.Cmif
+{
+    public static class ResultCmif
+    {
+        public const int ModuleSf = 10;
+
+        /// <summary>Error code: 2010-0011; Inner value: 0x160a</summary>
+        public static Result.Base CmifProxyAllocationFailed => new Result.Base(ModuleSf, 11);
+        /// <summary>Error code: 2010-0202; Inner value: 0x1940a</summary>
+        public static Result.Base InvalidCmifHeaderSize => new Result.Base(ModuleSf, 202);
+        /// <summary>Error code: 2010-0211; Inner value: 0x1a60a</summary>
+        public static Result.Base InvalidCmifInHeader => new Result.Base(ModuleSf, 211);
+        /// <summary>Error code: 2010-0212; Inner value: 0x1a80a</summary>
+        public static Result.Base InvalidCmifOutHeader => new Result.Base(ModuleSf, 212);
+        /// <summary>Error code: 2010-0221; Inner value: 0x1ba0a</summary>
+        public static Result.Base UnknownMethodId => new Result.Base(ModuleSf, 221);
+        /// <summary>Error code: 2010-0231; Inner value: 0x1ce0a</summary>
+        public static Result.Base InvalidInRawSize => new Result.Base(ModuleSf, 231);
+        /// <summary>Error code: 2010-0232; Inner value: 0x1d00a</summary>
+        public static Result.Base InvalidOutRawSize => new Result.Base(ModuleSf, 232);
+        /// <summary>Error code: 2010-0235; Inner value: 0x1d60a</summary>
+        public static Result.Base InvalidInObjectCount => new Result.Base(ModuleSf, 235);
+        /// <summary>Error code: 2010-0236; Inner value: 0x1d80a</summary>
+        public static Result.Base InvalidOutObjectCount => new Result.Base(ModuleSf, 236);
+        /// <summary>Error code: 2010-0239; Inner value: 0x1de0a</summary>
+        public static Result.Base InvalidInObject => new Result.Base(ModuleSf, 239);
+        /// <summary>Error code: 2010-0261; Inner value: 0x20a0a</summary>
+        public static Result.Base TargetObjectNotFound => new Result.Base(ModuleSf, 261);
+        /// <summary>Error code: 2010-0301; Inner value: 0x25a0a</summary>
+        public static Result.Base OutOfDomainEntry => new Result.Base(ModuleSf, 301);
+    }
+}
--- src/LibHac/Sf/ResultSf.cs
+++ src/LibHac/Sf/ResultSf.cs
@@ -21,35 +21,13 @@ namespace LibHac.Sf
         public static Result.Base NotSupported => new Result.Base(ModuleSf, 1);
         /// <summary>Error code: 2010-0003; Inner value: 0x60a</summary>
         public static Result.Base PreconditionViolation => new Result.Base(ModuleSf, 3);
-        /// <summary>Error code: 2010-0202; Inner value: 0x1940a</summary>
-        public static Result.Base InvalidHeaderSize => new Result.Base(ModuleSf, 202);
-        /// <summary>Error code: 2010-0211; Inner value: 0x1a60a</summary>
-        public static Result.Base InvalidInHeader => new Result.Base(ModuleSf, 211);
-        /// <summary>Error code: 2010-0221; Inner value: 0x1ba0a</summary>
-        public static Result.Base UnknownCommandId => new Result.Base(ModuleSf, 221);
-        /// <summary>Error code: 2010-0232; Inner value: 0x1d00a</summary>
-        public static Result.Base InvalidOutRawSize => new Result.Base(ModuleSf, 232);
-        /// <summary>Error code: 2010-0235; Inner value: 0x1d60a</summary>
-        public static Result.Base InvalidNumInObjects => new Result.Base(ModuleSf, 235);
-        /// <summary>Error code: 2010-0236; Inner value: 0x1d80a</summary>
-        public static Result.Base InvalidNumOutObjects => new Result.Base(ModuleSf, 236);
-        /// <summary>Error code: 2010-0239; Inner value: 0x1de0a</summary>
-        public static Result.Base InvalidInObject => new Result.Base(ModuleSf, 239);
-        /// <summary>Error code: 2010-0261; Inner value: 0x20a0a</summary>
-        public static Result.Base TargetNotFound => new Result.Base(ModuleSf, 261);
-        /// <summary>Error code: 2010-0301; Inner value: 0x25a0a</summary>
-        public static Result.Base OutOfDomainEntries => new Result.Base(ModuleSf, 301);
 
-        /// <summary>Error code: 2010-0800; Range: 800-899</summary>
-        public static Result.Base.Abstract RequestContextChanged { [MethodImpl(MethodImplOptions.AggressiveInlining)] get => new Result.Base.Abstract(ModuleSf, 800, 899); }
-            /// <summary>Error code: 2010-0801; Range: 801-809</summary>
-            public static Result.Base.Abstract RequestInvalidated { [MethodImpl(MethodImplOptions.AggressiveInlining)] get => new Result.Base.Abstract(ModuleSf, 801, 809); }
-                /// <summary>Error code: 2010-0802; Inner value: 0x6440a</summary>
-                public static Result.Base RequestInvalidatedByUser => new Result.Base(ModuleSf, 802);
+        /// <summary>Error code: 2010-0010; Range: 10-19; Inner value: 0x140a</summary>
+        public static Result.Base MemoryAllocationFailed => new Result.Base(ModuleSf, 10, 19);
 
-            /// <summary>Error code: 2010-0811; Range: 811-819</summary>
-            public static Result.Base.Abstract RequestDeferred { [MethodImpl(MethodImplOptions.AggressiveInlining)] get => new Result.Base.Abstract(ModuleSf, 811, 819); }
-                /// <summary>Error code: 2010-0812; Inner value: 0x6580a</summary>
-                public static Result.Base RequestDeferredByUser => new Result.Base(ModuleSf, 812);
+        /// <summary>Error code: 2010-0811; Range: 811-819</summary>
+        public static Result.Base.Abstract RequestDeferred { [MethodImpl(MethodImplOptions.AggressiveInlining)] get => new Result.Base.Abstract(ModuleSf, 811, 819); }
+            /// <summary>Error code: 2010-0812; Inner value: 0x6580a</summary>
+            public static Result.Base RequestDeferredByUser => new Result.Base(ModuleSf, 812);
     }
 }
