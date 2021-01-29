e5291eb06aa7931a8e73efc32403a1e8c30516de
Alex Barney <thealexbarney@gmail.com>
2021-01-29 01:03:23 -0700

    Fix IFatFileSystemCreator

---
+++ src/LibHac/Fat/FatFormatParam.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibHac.Fat
+{
+    public struct FatFormatParam
+    {
+        public bool IsSdCard;
+        public uint ProtectedAreaSectors;
+        public Result WriteVerifyErrorResult;
+        public Memory<byte> WorkBuffer;
+    }
+}
