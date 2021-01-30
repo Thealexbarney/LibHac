e9f518ddc1490eabeb0092d75b73ffa365d3a96e
Alex Barney <thealexbarney@gmail.com>
2021-01-30 02:29:13 -0700

    Convert tabs to spaces because spaces are better. Fight me.
    No idea how this one file got formatted with tabs.

--- src/LibHac/Os/Impl/InternalCriticalSection-os.net.cs
+++ src/LibHac/Os/Impl/InternalCriticalSection-os.net.cs
@@ -2,35 +2,35 @@
 
 namespace LibHac.Os.Impl
 {
-	internal struct InternalCriticalSectionImpl
-	{
-		private object _obj;
+    internal struct InternalCriticalSectionImpl
+    {
+        private object _obj;
 
-		public void Initialize()
-		{
-			_obj = new object();
-		}
+        public void Initialize()
+        {
+            _obj = new object();
+        }
 
-		public void FinalizeObject() { }
+        public void FinalizeObject() { }
 
-		public void Enter()
-		{
-			Monitor.Enter(_obj);
-		}
+        public void Enter()
+        {
+            Monitor.Enter(_obj);
+        }
 
-		public bool TryEnter()
-		{
-			return Monitor.TryEnter(_obj);
-		}
+        public bool TryEnter()
+        {
+            return Monitor.TryEnter(_obj);
+        }
 
-		public void Leave()
-		{
-			Monitor.Exit(_obj);
-		}
+        public void Leave()
+        {
+            Monitor.Exit(_obj);
+        }
 
-		public bool IsLockedByCurrentThread()
-		{
-			return Monitor.IsEntered(_obj);
-		}
-	}
+        public bool IsLockedByCurrentThread()
+        {
+            return Monitor.IsEntered(_obj);
+        }
+    }
 }
