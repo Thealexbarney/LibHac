fe6bc70a662c3d4ba00331bca24c04654101e9f3
Alex Barney <thealexbarney@gmail.com>
2021-03-11 23:02:11 -0700

    Remove some old logging classes

--- src/LibHac/Fs/IAccessLog.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace LibHac.Fs
-{
-    public interface IAccessLog
-    {
-        void Log(Result result, System.TimeSpan startTime, System.TimeSpan endTime, int handleId, string message, [CallerMemberName] string caller = "");
-    }
-}
