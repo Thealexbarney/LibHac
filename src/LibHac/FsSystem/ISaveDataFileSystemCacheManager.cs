f1caabf82bc3b22e907a75acaca7d8a7eb36b113
Alex Barney <thealexbarney@gmail.com>
2021-05-06 01:02:30 -0700

    Add SaveDataFileSystemCacheManager

--- src/LibHac/FsSystem/ISaveDataFileSystemCacheManager.cs
+++ src/LibHac/FsSystem/ISaveDataFileSystemCacheManager.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using LibHac.Fs;
+using LibHac.Fs.Fsa;
 using LibHac.FsSystem.Save;
 
 namespace LibHac.FsSystem
 {
     public interface ISaveDataFileSystemCacheManager : IDisposable
     {
-        bool GetCache(out ReferenceCountedDisposable<SaveDataFileSystem> fileSystem, SaveDataSpaceId spaceId, ulong saveDataId);
+        bool GetCache(out ReferenceCountedDisposable<IFileSystem> fileSystem, SaveDataSpaceId spaceId, ulong saveDataId);
         void Register(ReferenceCountedDisposable<ApplicationTemporaryFileSystem> fileSystem);
         void Register(ReferenceCountedDisposable<SaveDataFileSystem> fileSystem);
         void Unregister(SaveDataSpaceId spaceId, ulong saveDataId);
+        
+        // LibHac addition
+        void Register(ReferenceCountedDisposable<DirectorySaveDataFileSystem> fileSystem);
     }
 }
