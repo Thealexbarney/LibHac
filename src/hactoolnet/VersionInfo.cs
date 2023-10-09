8de9980160a47e077b6b4fde1a0f45b966cf4921
Alex Barney <thealexbarney@gmail.com>
2023-10-08 18:16:58 -0700

    Bump version to 0.19.0

--- src/hactoolnet/VersionInfo.cs
+++ src/hactoolnet/VersionInfo.cs
@@ -2,7 +2,7 @@
 {
     internal static class VersionInfo
     {
-        public static string Version => "0.18.0";
+        public static string Version => "0.19.0";
         public static string CommitTime => "";
         public static string CommitHash => "";
     }
